60b5516020a0786159d7a2a4648c124e8e7dd155
Lucas Zacharewicz <lucas.zacharewicz@gmail.com>
2023-07-24 22:48:53 -0400

    bug: Testing semantic-release auto versioning

--- getcomics/__init__.py
+++ getcomics/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
+__version__ = "0.0.0"
+
 
 def __init__():
     pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+from setuptools import setup
+
+__version__ = "0.0.0"
+
+setup(
+    name="get-comics",
+    version=__version__,
+)
