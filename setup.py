9f37714a9d5e73322b988a950a9256a8d89e4dce
Lucas Zacharewicz <lucas.zacharewicz@gmail.com>
2023-07-24 23:10:41 -0400

    feat: Testing version incrementation

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@
 
 from __future__ import annotations
 
+from sys import argv
+
 from setuptools import setup
 
 __version__ = "0.0.0"
+
+try:
+    from semantic_release import setup_hook
+
+    setup_hook(argv)
+except ImportError:
+    pass
 
 setup(
     name="get-comics",
